c864034b2cbe813bac52bbe358b8a78822968a39
Vinadeltasari <127222366+Vinadeltasari@users.noreply.github.com>
2023-03-15 12:20:46 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/pbo_2211083046/PBO_2211083046.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo_2211083046;
+
+/**
+ *
+ * @author HP
+ */
+public class PBO_2211083046 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
