31dc775f76d58f94a3a1c228c484cf338946e75b
Vinadeltasari <127222366+Vinadeltasari@users.noreply.github.com>
2023-03-31 23:06:35 +0700

    upload projek

---
+++ src/main/java/vina300323/casting.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vina300323;
+
+/**
+ *
+ * @author HP
+ */
+public class casting {
+    public static void main (String [] args){
+       /* char valChar = 'A';
+        int valInt = valChar;
+        System.out.print( valInt ); */
+      
+    double valDouble = 10.12;
+    int valInt = (int)valDouble; //men-convert valDouble ke tipe int
+    double x = 10.2;
+    int y = 2;
+    int result = (int)(x/y); //hasil typecast operasi ke int 
+
+    }
+}
+
